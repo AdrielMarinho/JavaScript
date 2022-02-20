7ca381713c13e5f4bf2280b3d893eb55964e714f
AdrielMarinho <adrielqmarinho@hotmail.com>
2022-02-20 18:21:55 -0300

    att

--- Aula12Ex/Ex014/script.js
+++ Aula12Ex/Ex014/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('image')
+    var data = new Date()
+    var horaAtual = data.getHours() 
+    msg.innerHTML = `Agora são ${horaAtual} horas.`
+    if (horaAtual >= 0 && horaAtual < 12) {
+        // Dia
+        img.src = 'fotomanha.jpg'
+    } else if (horaAtual < 18) {
+        // Tarde
+        img.src = 'fototarde.jpg'
+    } else {
+        // Noite
+        img.src = 'fotonoite.jpg'
+    }
+}
